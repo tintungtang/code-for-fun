867635bf02df01b6ce9e3af7165f658b3e332b88
tintungtang <tin.thanh.thai@gmail.com>
2025-03-17 12:20:19 +0700

    bug: workaround testing for JWT cookie issue

--- YoTeams/YoTeams/Services/TeamService.cs
+++ YoTeams/YoTeams/Services/TeamService.cs
@@ -20,6 +20,9 @@ public class TeamService
 
     public async Task<List<Team>> GetTeamsAsync()
     {
+        
+        _httpClient.DefaultRequestHeaders.Add("Cookie",
+            "AUTH_JWT=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2N2Q2OGM5NTU4NDQ2ZDI4MTQwYzIyMDgiLCJ1bmlxdWVfbmFtZSI6ImFkbWluIiwianRpIjoiZTRlNzJmNGYtZDQ5OS00MGZmLTkzNGUtMzc3OTk2MTBkNjg0IiwiZXhwIjoxNzQyMTkxNzQ4LCJpc3MiOiJ5b3VyLWFwcCIsImF1ZCI6InlvdXItY2xpZW50In0.0jQxpyORuD8r2-5LZnBf9Ox-nEpEoSdnW--YjBM96kY");
         return await _httpClient.GetFromJsonAsync<List<Team>>( "teams") ?? new List<Team>();
     }
 }
